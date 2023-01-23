e31a93fd670398fb3fd87316cc00cbfdbadb69d7
sanghyuk0612 <hwhwhw944@gmail.com>
2023-01-24 00:42:56 +0900

    Create 4344.c

---
+++ 4344.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main(void) {
+    int n;
+    int s;
+    int cnt=0;
+    float avg;
+    scanf("%d", &n);
+
+    int score[1000];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &s);
+        for (int j = 0; j < s; j++) {
+            scanf("%d", &score[j]);
+            avg += score[j];
+        }
+        avg = avg / n;
+        for (int j = 0; j < s; j++) {
+            if (score[j] < avg) {
+                cnt++;
+            }
+            printf("%f", cnt / j);
+        }
+    
+    }
+    
+
+    
+    return 0;
+}
