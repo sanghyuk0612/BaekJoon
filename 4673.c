2542835a218ce4f276f265b5ea00bcd691439b24
sanghyuk0612 <hwhwhw944@gmail.com>
2023-01-28 15:17:20 +0900

    Update 4673.c

--- 4673.c
+++ 4673.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int len(int n) {
     int cnt = 0;
-    for (int i = 0; n > 0; n++) {
+    while (n != 0) {
         n = n / 10;
         cnt++;
     }
@@ -10,12 +10,12 @@ int len(int n) {
 int main(void) {
     int num[10000];
     int l;
-    for (int i = 0; i < 100; i++) {
+    for (int i = 0; i < 10000; i++) {
         num[i] = i + 1;
     }
-    for (int i = 0; i < 100; i++) {
+    for (int i = 0; i < 10000; i++) {
         l = len(num[i]);
-        printf("%d", l);
+
     }
     return 0;
 }
