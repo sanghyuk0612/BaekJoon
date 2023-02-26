17e7c690c975fc935ad908579b5f8d5545fb9cd7
sanghyuk0612 <122794293+sanghyuk0612@users.noreply.github.com>
2023-02-27 01:30:47 +0900

    [level 2] Title: H-Index, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/42747. H－Index/H－Index.py
+++ 프로그래머스/lv2/42747. H－Index/H－Index.py
@@ -1,16 +1,16 @@
 def solution(citations):
     answer = 0
-    # 1. citations을 오름차순으로 정렬합니다.
+    
     citations.sort()
-    # 2. citations 길이 n을 구합니다.
-    n = len(citations)
-    # 3. 0~n까지 다음을 반복합니다
-    for i in range(n):
-        # 1. hIndex는 n-i입니다.
-        hIndex = n-i
+    
+    
+    
+    for i in range(len(citations)):
+        
+        h = len(citations)-i
         # 2. citations[i]가 hIndex보다 크거나 같으면, answer에 hIndex를 저장하고 반복을 멈춥니다. 
-        if citations[i] >= hIndex:
-            answer = hIndex
+        if citations[i] >= h:
+            answer = h
             break
     
     return answer
