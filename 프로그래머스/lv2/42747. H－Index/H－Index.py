66ea8879523e5a60834e063f14e268f74cae078d
sanghyuk0612 <122794293+sanghyuk0612@users.noreply.github.com>
2023-02-27 17:25:14 +0900

    [level 2] Title: H-Index, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/42747. H－Index/H－Index.py
+++ 프로그래머스/lv2/42747. H－Index/H－Index.py
@@ -1,14 +1,10 @@
 def solution(citations):
     answer = 0
     
-    citations.sort()
-    
-    
-    
+    citations=sorted(citations)
     for i in range(len(citations)):
         
         h = len(citations)-i
-        # 2. citations[i]가 hIndex보다 크거나 같으면, answer에 hIndex를 저장하고 반복을 멈춥니다. 
         if citations[i] >= h:
             answer = h
             break
